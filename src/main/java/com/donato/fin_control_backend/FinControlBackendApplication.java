51c2013a1464ed4bcad2147295fee18cd43cc7e2
Joao Donato <joaovictor_gd@hotmail.com>
2025-11-09 09:22:24 -0300

    first commit

---
+++ src/main/java/com/donato/fin_control_backend/FinControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.donato.fin_control_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinControlBackendApplication.class, args);
+	}
+
+}
